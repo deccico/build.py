31c87051f88a10b667c3edcf080b92d9156d6f41
Adrián Deccico <deccico@gmail.com>
2016-08-30 11:25:53 +1000

    detecting changes

--- buildpy/build.py
+++ buildpy/build.py
@@ -2,12 +2,20 @@
 
 import argparse
 import logging
+import subprocess
+import sys
 
 
 APP = "Build.py"
-VERSION = '0.0.2'
+VERSION = '0.1.0'
 DESCRIPTION = 'Pyhton build tool.'
 LONG_DESCRIPTION = '"He must trust, and he must have faith. And so he builds, because what is building, and rebuilding and rebuilding again, but an act of faith?" Dave Eggers'
+
+if sys.version_info <= (2, 4) or sys.version_info >= (3, 0):
+    error = "ERROR: your Python version is not supported. Please use build.py with Python 2.7.x"
+    print >> sys.stderr, error
+    sys.exit(1)
+
 
 FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 logging.basicConfig(level=logging.INFO,
@@ -18,24 +26,91 @@ logging.basicConfig(level=logging.INFO,
 
 
 #-------------------------------------------------------
-def pre_build():
-    pass
+def run(cmd):
+    logging.info("running: {0}".format(cmd))
+    return subprocess.call(cmd, shell=True)
 
+
+def pre_build_checks():
+    logging.info("pre_build_checks checking...")
+    modified_files = not run('git status -s|grep -E "^?M "')
+    added_files = not run('git status -s|grep -E "^?A "')
+    if modified_files:
+        logging.error("You have uncommited, modified changes in your git repo.")
+        sys.exit(1)
+    if added_files:
+        logging.error("You have uncommited, added files in your git repo.")
+        sys.exit(1)
+
+def coverage():
+    logging.info("running code coverage..")
+    sys.exit(run("code_coverage_cmd"))
+
+def build(type, clean, build_arg, unit_tests):
+    logging.info("building... type:{0} clean:{1} build:{2} unit-test?:{3}".format(type, clean, build_arg, unit_tests))
+    sys.exit(run("gcc {0} {1} {2} {3}".format(type, clean, build_arg, unit_tests)))
+
+
+def validate_options(args):
+    #subcommands are mandatory in Python 2.x therefore we need to implement our own validation..
+    #this means we cannot call ./build.py -p or ./build.py -k alone. If we specify a subcommand, we must
+    #call it.
+    #When the issue below is fixed we could implement a positional argument (build?)
+    # with -c -u and -b as optional arguments.
+    # For more information: http://bugs.python.org/issue9253
+    for opt in ['args.clean', 'args.build', 'args.unit_tests']:
+        if args.type=='None' and eval(opt):
+            raise RuntimeError("You need to specify the build type option (-t) along with --{0}".format(opt[5:]))
 
 def main():
-    # create the base parser with a subparsers argument
     parser = argparse.ArgumentParser(prog=APP.lower(), \
                                     description=DESCRIPTION)
+
     parser.add_argument('--version', action='version', version=APP + " " + VERSION)
-    subparsers = parser.add_subparsers()
-    
-    parser_bewitch = subparsers.add_parser('pre-build checks', help='Runs any pre build checks required.')
-    parser_bewitch.set_defaults(func=pre_build)
+
+    #-p section
+    parser.add_argument("-p", "--pre_build_checks", default=False, action="store_true",
+                        help="Runs any pre build checks required.")
+
+    #-k code coverage section
+    parser.add_argument("-k", "--coverage", default=False, action="store_true",
+                        help="Coverage Reporting. Compiles binaries with coverage flags to generate "
+                             "coverage and static analysis data for sonar.")
+
+    #build section subcommand and options
+    parser.add_argument("-t", "--type", type=str, default='None', choices=["debug", "prod", "profiling"],
+                        help="Defines build flavor type.")
+    parser.add_argument("-c", "--clean", default=False, action="store_true",
+                        help="Deletes any meta files which are generated as part of a compile for given "
+                             "type eg: intermediate object files and directories. Including delete the "
+                             "existing SHIP folder. Must no delete non build generated files such as "
+                             "source files not yet commited, etc.")
+    parser.add_argument("-b", "--build", default=False, action="store_true",
+                        help="Compiles the given target. All required binaries must be compiled, this included "
+                             "all unit test binaries. All files must be put in the define structure.")
+    parser.add_argument("-u", "--unit-tests", default=False, action="store_true",
+                              help="Run all Unit tests from the defined unit-test deployment folder. "
+                                   "If the unit test doesn't exit it should print a warning and compile "
+                                   "(which will create the folder and binaries) then run the unit tests. "
+                                   "All unit tests are required to produce a xml file in the root of the "
+                                   "unit-test folder in junit xml format. One file per unit test binary is "
+                                   "required. Naming convention is <name of unit-test-binary>- results.xml")
 
     args = parser.parse_args()
-    args.func(args)
+
+    validate_options(args)
+
+    if args.pre_build_checks:
+        pre_build_checks()
+
+    if args.coverage:
+        coverage()
+
+    if args.type:
+        build(args.type, args.clean, args.build, args.unit_tests)
 
 if __name__ == "__main__":
     main()
+
 
 
--- readme.rst
+++ readme.rst
@@ -1,8 +1,10 @@
-#Goals:
+Goals:
+=======
 
 To provide a standard consistent interface to compile, test, package all developed software.
 
-##High level requirements:
+High level requirements:
+------------------------
 
 * Must be self-contained
 * Must return exist codes
@@ -14,8 +16,8 @@ mocked.
 
 All deploy-able files will be in a defined folder and the predefined structure.
 
-##Deployment folder specification
-
+Deployment folder specification
+---------------------------------
 At the end of every compile the following folder structure is required.
 
 At the root of the checkout. which build.py exists, Structure:
@@ -30,59 +32,13 @@ can be multiple subfolders, must be target machine required structure.)
         |—[configuration] (all run time configuration)
         |—[database] (all database scripts)
 
-###build.py CLI specifications
+build.py CLI specifications
+----------------------------
 
-|Action | Flag | Function|Requirements|
-|Pre build checks| -p | Runs any pre build checks required.
-eg: Checks if dependencies are installed, has port
-access to required systems, etc
-Usage
-build.py -p
-Optional: Nice to have would be to check if you have
-un-commit code or files which have not been added to
-source control and warn the user. Pause for 10 seconds and run. |None|
-|Define
-target|-t|
-Defines build flavor type.
-eg: debug | prod | profiling | etc
-Usage
-build.py -t debug -b -u
-This example builds (possibly an incremental as not -c
-is supplied) then runs the unit test.|
-* Mandatory
-* Must have another action associated.|
-|Clean|-c|Deletes any meta files which are generated as part of
-a compile for given type
-eg: intermediate object files and directories. Including
-delete the existing SHIP folder
-Must no delete non build generated file such as source
-files not yet commit to etc.
-Usage
-build.py -t debug -c
-This example will clean the target compile.|-t flag
-Build|-b|Compiles the given target.
-All required binaries must be compiled, this included all
-unit test binaries.
-All files must be put in the defined structure
-Usage
-build.py -t debug -c -b
-This example will clean and compile a debug target|-t flag|
-Unit Test|-u|Run all Unit tests from the defined unit-test deployment
-folder.
-If the unit test doesn't exit it should print a warning and
-compile (which will create the folder and binaries)
-then run the unit tests.
-All unit tests are required to produce a xml file in the
-root of the unit-test folder in junit xml format.
-One file per unit test binary is required.
-Naming convention is <name of unit-test-binary>-
-results.xml
-Usage
-build.py -t debug -u
-This example will run all unit tests in the SHIP\unit-test\
-folder|-t flag|
-Coverage Reporting|-k|Compiles a binaries with coverage flags to generate
-coverage and static analysis data for sonar.|
-None.
-Self
-contained.|
+For the complete list of options, please run:
+
+::
+./build.py --help
+or
+./build.py build --help
+
